6c7e736e2212ba2635b2181d048861dfeaa04de2
adrianomts7 <161032614+adrianomts7@users.noreply.github.com>
2025-05-01 15:49:10 -0300

    Criando o arquivo app

---
+++ app.js
@@ -0,0 +1,21 @@
+import dotenv from "dotenv";
+import express from "express";
+
+dotenv.config();
+
+class App {
+  constructor() {
+    this.app = express();
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares() {
+    this.app.use(express.urlencoded({ extended: true }));
+    this.app.use(express.json());
+  }
+
+  routes() {}
+}
+
+export default new App();
